4114721a3ece562e2b04a390eaa753286f66a39c
stabla <contact@guillaumebonnet.fr>
2017-08-23 23:22:35 +0200

    adding config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// TWITTER APP CONFIGURATION
+
+module.exports = {
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+}
