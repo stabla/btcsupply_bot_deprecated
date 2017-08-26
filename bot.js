ad5c44adda52a385d36d9ac37e119b91ec83ea69
Guillaume Bonnet <giomebs@gmail.com>
2017-08-26 11:19:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ var differentSupply = function (currentSupply, difference, price) {
     var messages = " There's new " + format(difference) + " Bitcoin generated.\n \n It represents $" + format((difference * price)) + " (At $" + format(price) + " per $BTC #Bitcoin #BTC) \n New Supply : " + format(currentSupply) + "";
 
     console.log(messages);
-    /*postTweet(messages);*/
+    postTweet(messages);
 }
 // ==========================================================================//
 
