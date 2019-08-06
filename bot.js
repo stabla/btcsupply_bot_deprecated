60f92a7ecd3c9aabd56625a26967de32f58ee5da
Guillaume Bonnet <stabla@protonmail.com>
2019-08-06 20:48:16 +0200

    updating to coinmarketcap new API

--- bot.js
+++ bot.js
@@ -1,31 +1,32 @@
 var twit = require('twit'),
     config_tw = require('./config');
+var rp = require('request-promise');
 
 var Twitter = new twit(config_tw);
 var lastSupply = 0;
-
 
 // ==========================================================================//
 /*
         Post a tweet
 */
 var postTweet = function (messages) {
-    var params = {
-        status: messages
-    }
-
-    // Update status: post the tweet
-    Twitter.post('statuses/update', params, function (error, tweet, response) {
-        if (error) {
-            console.log(error);
-            console.log('Tweet already posted');
-        } else {
-		console.log('Tweeted correctly posted.');
-	}
-    });
-
-};
-
+        var params = {
+            status: messages
+        }
+    
+        // Update status: post the tweet
+        Twitter.post('statuses/update', params, function (error, tweet, response) {
+            console.log("Tweet correctly.");
+            if (error) {
+                console.log(error);
+                console.log('Tweet already posted');
+            } else {
+                    console.log('Tweeted correctly posted.');
+            }
+        });
+    
+    };
+    
 
 // ==========================================================================//
 /*
@@ -40,6 +41,7 @@ var getInlastTweet = function () {
         if (error) {
             console.log('Erreur');
         }
+        console.log("getInLastTweet.");
         var a, b;
         // get the text property from the json
         a = data[0].text;
@@ -47,6 +49,8 @@ var getInlastTweet = function () {
         b = a.substr(a.length - 30);
         // delete the ',' and return just the entire number, in int
         lastSupply = parseInt(b.split(',').join(''));
+
+        console.log(lastSupply);
     });
 };
 
@@ -58,9 +62,9 @@ var getInlastTweet = function () {
         Function to format numbers
 */
 var format = function (x) {
-    parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
+        parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
 };
 
 /*
@@ -71,8 +75,7 @@ var differentSupply = function (currentSupply, difference, price, percentage) {
 
         postTweet(messages);
 };
- // ==========================================================================//
-
+// ==========================================================================//
 
 // ==========================================================================//
 /*
@@ -80,54 +83,53 @@ var differentSupply = function (currentSupply, difference, price, percentage) {
 */
 var makeRequest = function () {
 
-    getInlastTweet();
-    const https = require('https');
-    const options = {
-        host: 'api.coinmarketcap.com',
-        port: 443,
-        path: '/v2/ticker/1/',
-        method: 'GET'
+        getInlastTweet();
+        const https = require('https');
+        /* Example in Node.js ES6 using request-promise */
+        const requestOptions = {
+                method: 'GET',
+                uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=1&convert=USD',
+                headers: {
+                'X-CMC_PRO_API_KEY': '6a100269-8e51-4e49-b5a1-aed1fe1fe889'
+                },
+                json: true,
+                gzip: true
+        };
+      
+        var callCoinmarketcap = function() {
+                rp(requestOptions).then(response => {
+                        var new_supply = response.data[1].total_supply;
+                        var current_price = response.data[1].quote.USD.price;
+
+                        if (lastSupply == 0) {
+                                console.log("lastSupply === 0.");
+                                setTimeout(function () {
+
+                                if (new_supply >= lastSupply) {
+                                        console.log("new_supply >= lastSupply ");
+                                        console.log('new_supply: ' + new_supply);
+                                        console.log('lastSupply: ' + lastSupply);
+                                        console.log('difference: ' + (new_supply - lastSupply));
+
+                                        // Call function and tweet about it
+                                        difference = (new_supply - lastSupply);
+
+                                        percentage = ((new_supply/21000000)*100).toFixed(2);
+
+                                        lastSupply = 0; // initialize value for next tweet
+                                        differentSupply(new_supply, difference, current_price, percentage);
+                                }
+
+                                }, 10000);
+                        }
+                }).catch((err) => {
+                console.log('API call error:', err.message);
+                });
+        };
+    
+        callCoinmarketcap();
     };
-
-    var callback = function (response) {
-        var a = new Array();
-
-        // Make a request to get the json
-        response.on('data', function (d) {
-            a += d;
-        });
-
-        // When the request is ended, format correctly and show the value.
-        response.on('end', function () {
-            b = JSON.parse(a);
-
-            var newSupply = parseInt(b.data.total_supply); // Returned value from the request
-            var priceUSD = parseInt(b.data.quotes.USD.price);
-
-            if (lastSupply == 0) {
-                setTimeout(function () {
-
-                    if (newSupply >= lastSupply) {
-                        console.log('NewSupply: ' + newSupply);
-                        console.log('lastSupply: ' + lastSupply);
-                        console.log('difference: ' + (newSupply - lastSupply));
-
-                        // Call function and tweet about it
-                        difference = (newSupply - lastSupply);
-
-                        percentage = ((newSupply/21000000)*100).toFixed(2);
-
-                        lastSupply = 0; // initialize value for next tweet
-                        differentSupply(newSupply, difference, priceUSD, percentage);
-                    }
-
-                }, 10000);
-            }
-        }); // end of response
-    };
-
-    var req = https.request(options, callback).end();
-};
-
-// Launch the Coinmarketcap request to get BTC supply each 12 hours (43 200 000 ms)
+    
+// Launch the Coinmarketcap request to get BTC supply each 12 hours (43200000 ms)
+console.log("Runned.");
 setInterval(makeRequest, 43200000);
